ac050fa1e809de1da292c814931bc7ca8a0229ce
Padma-1 <65107839+Padma-1@users.noreply.github.com>
2022-08-17 18:28:22 +0530

    Update WomenEmpProjectApplicationTests.java

--- Backend-Eclipse/src/test/java/com/wipro/velocity/empowerher/WomenEmpProjectApplicationTests.java
+++ Backend-Eclipse/src/test/java/com/wipro/velocity/empowerher/WomenEmpProjectApplicationTests.java
@@ -3,8 +3,7 @@ package com.wipro.velocity.empowerher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootConfiguration
-@ContextConfiguration
+
 @SpringBootTest
 class WomenEmpProjectApplicationTests 
 {
